b2cdc8d269cde3a15b53e256969f266ef3ce111c
Kiiroi <sammydevjs@gmail.com>
2022-12-07 07:53:59 -0300

    menu lateral inserido com animacoes e titulos dinamicos

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const conteudoDetalhes = document.querySelectorAll('.conteudo');
 const botaoCopiar = document.querySelectorAll('.copiar');
 const codigoCopiar = document.querySelectorAll('.codigo');
 const imgCopiar = document.querySelectorAll('.imgCopiar');
+const caixa = document.querySelector('#menuLateralReact');
+const titulos = document.querySelectorAll('.tituloSecaoReact');
+const ulDoIndice = document.querySelector('.primeiraLista');
+const setaMenuLateral = document.querySelector('.setaMenuLateral');
+
 // FUNÇÃO PARA O BOTÃO DETALHES ABRIR E FECHAR O CONTEÚDO
 for (let i = 0; i < botaoDetalhes.length; i++) {
     botaoDetalhes[i].addEventListener('click', ()=> {
@@ -35,3 +40,39 @@ for (let i = 0; i < botaoCopiar.length; i++) {
       });
     });
 }
+
+// CAIXA DE ÍNDICE
+
+
+// CAPTURANDO TITULOS E INSERINDO AQUI
+for (let i = 0; i < titulos.length; i++) {
+  let li = document.createElement('li');
+  ulDoIndice.appendChild(li);
+  let a = document.createElement('a');
+  li.appendChild(a);
+  a.href = `#${titulos[i].id}`;
+  a.innerText = titulos[i].innerText;
+}
+
+// GUARDANDO O MENU E MOVENDO A SETA
+setaMenuLateral.addEventListener('click', ()=> {
+  if(caixa.classList.contains('aberta')){
+    caixa.classList.remove('aberta');
+    caixa.classList.add('fechada');
+    setaIMG.style.animationPlayState = "running";
+    setaIMG.style.animationName = 'movimentoSeta';
+    caixa.style.animationPlayState = 'running';
+    caixa.style.animationName = 'guardandoCaixa';
+  } else {
+    caixa.classList.add('aberta');
+    caixa.classList.remove('fechada');
+    setaIMG.style.animationPlayState = "running";
+    setaIMG.style.animationName = 'movimentoSeta2';
+    caixa.style.animationPlayState = 'running';
+    caixa.style.animationName = 'aparecendoCaixa';
+  }
+})
+
+// ABRINDO E FECHANDO A CAIXA DE ÍNDICE AO CLICAR EM SETA
+
+// transform: translate(-100%, -50%);
